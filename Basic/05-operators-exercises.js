d21b665df414a99bfec1ec350fb804098b006985
samuel-un <samuelun00@gmail.com>
2024-08-13 03:59:20 +0200

    Ejercicios resueltos por mi.

--- Basic/05-operators-exercises.js
+++ Basic/05-operators-exercises.js
@@ -6,21 +6,50 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 
 // 1. Crea una variable para cada operación aritmética
 
+let a = 10, b = 50
+
+let suma = a + b
+let resta = a - b
+let multiplicacion = a * b
+let division = a / b
+let modulo = a % b
+let exponente = a ** b
+
 // 2. Crea una variable para cada tipo de operación de asignación,
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
 
-// 3. Imprime 5 comparaciones verdades con diferentes operadores de comparación
+let myVariable = 7
 
-// 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+myVariable += 3
+myVariable -= 3
+myVariable *= 3
+myVariable /= 3
+myVariable %= 3
+myVariable **= 3
 
 // 5. Utiliza el operador lógico and
 
+console.log((10 > 5) && (5 < 10))
+
 // 6. Utiliza el operador lógico or
+
+    console.log((10 > 5) || (10 < 5))
 
 // 7. Combina ambos operadores lógicos
 
+    console.log((10 == "10" && true == 1) || 0 == false)
+
 // 8. Añade alguna negación
+
+    console.log(!(10 == 10 && 10 == 10) || 10 != 10)
 
 // 9. Utiliza el operador ternario
 
+let soyEspañol = true
+
+soyEspañol ? console.log("Viva España!") : console.log("No seras francés no?")
+
 // 10. Combina operadores aritméticos, de comparáción y lógicas
+
+let resultado = (a + b) == 60 && (a + b) < 100 || (a + b) != 0 || !soyEspañol === false
+console.log(resultado)
