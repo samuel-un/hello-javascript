efc12afcf9f03d04737802f2d5f16840a0322ee0
samuel-un <samuelun00@gmail.com>
2024-08-13 04:00:41 +0200

    Ejercicios resueltos por mi.

--- Basic/05-operators-exercises.js
+++ Basic/05-operators-exercises.js
@@ -6,30 +6,30 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 
 // 1. Crea una variable para cada operación aritmética
 
-let a = 10, b = 50
+    let a = 10, b = 50
 
-let suma = a + b
-let resta = a - b
-let multiplicacion = a * b
-let division = a / b
-let modulo = a % b
-let exponente = a ** b
+    let suma = a + b
+    let resta = a - b
+    let multiplicacion = a * b
+    let division = a / b
+    let modulo = a % b
+    let exponente = a ** b
 
 // 2. Crea una variable para cada tipo de operación de asignación,
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
 
-let myVariable = 7
+    let myVariable = 7
 
-myVariable += 3
-myVariable -= 3
-myVariable *= 3
-myVariable /= 3
-myVariable %= 3
-myVariable **= 3
+    myVariable += 3
+    myVariable -= 3
+    myVariable *= 3
+    myVariable /= 3
+    myVariable %= 3
+    myVariable **= 3
 
 // 5. Utiliza el operador lógico and
 
-console.log((10 > 5) && (5 < 10))
+    console.log((10 > 5) && (5 < 10))
 
 // 6. Utiliza el operador lógico or
 
@@ -45,11 +45,11 @@ console.log((10 > 5) && (5 < 10))
 
 // 9. Utiliza el operador ternario
 
-let soyEspañol = true
+    let soyEspañol = true
 
-soyEspañol ? console.log("Viva España!") : console.log("No seras francés no?")
+    soyEspañol ? console.log("Viva España!") : console.log("No seras francés no?")
 
 // 10. Combina operadores aritméticos, de comparáción y lógicas
 
-let resultado = (a + b) == 60 && (a + b) < 100 || (a + b) != 0 || !soyEspañol === false
-console.log(resultado)
+    let resultado = (a + b) == 60 && (a + b) < 100 || (a + b) != 0 || !soyEspañol === false
+    console.log(resultado)
