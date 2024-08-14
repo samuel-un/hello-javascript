e46da6fae6be1448fa7ec98130a3292aa2fda1bc
samuel-un <samuelun00@gmail.com>
2024-08-14 03:41:49 +0200

    Ejercicios resueltos por mi.

--- Basic/07-strings-exercises.js
+++ Basic/07-strings-exercises.js
@@ -5,21 +5,53 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 */
 
 // 1. Concatena dos cadenas de texto
+    let nombre = "Samuel"
+    let saludo = "Hola, " + nombre + " que tengas un buen dia!"
+
+    console.log(saludo)
 
 // 2. Muestra la longitud de una cadena de texto
 
+    let longitudSaludo = saludo.length
+
+    console.log(longitudSaludo + " caracteres tiene la cadena de texto.") 
+
 // 3. Muestra el primer y último carácter de un string
+
+    let primerCaracter = nombre.charAt(0)
+    let longitudNombre = nombre.length
+    let ultimoCaracter = nombre.charAt(longitudNombre - 1)
+
+    console.log("Primer caracter del nombre: " + primerCaracter)
+    console.log("Ultimo caracter del nombre: " + ultimoCaracter)
 
 // 4. Convierte a mayúsculas y minúsculas un string
 
+    console.log(nombre.toUpperCase()) 
+    console.log(nombre.toLowerCase())
+
 // 5. Crea una cadena de texto en varias líneas
+
+let mensaje = `Hola,
+buenos dias cliente.`
 
 // 6. Interpola el valor de una variable en un string
 
+    console.log(`Hola ${nombre}! que tengas un buen dia.`)
+
 // 7. Reemplaza todos los espacios en blanco de un string por guiones
 
+    console.log(mensaje.replaceAll(" ", "-"))
+
 // 8. Comprueba si una cadena de texto contiene una palabra concreta
+    let palabraAbuscar = "samuel"
+    let contieneMensaje = mensaje.toLowerCase.includes(palabraAbuscar.toLowerCase)
+    contieneMensaje ? console.log("Si contiene la palabra: " + palabraAbuscar) : console.log("No contiene la palabra: " + palabraAbuscar)
 
 // 9. Comprueba si dos strings son iguales
 
-// 10. Comprueba si dos strings tienen la misma longitud
+    console.log(nombre == saludo)
+
+    // 10. Comprueba si dos strings tienen la misma longitud
+
+    console.log(nombre.length == saludo.length)
--- Basic/09-conditionals-exercises.js
+++ Basic/09-conditionals-exercises.js
@@ -8,23 +8,181 @@ https://www.twitch.tv/videos/2206228701?t=00h16m02s
 
 // 1. Imprime por consola tu nombre si una variable toma su valor
 
+    let nombrePreguntado = true
+
+    if (nombrePreguntado) {
+        console.log("Samuel")
+    }
+
 // 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
 
+    let usuario = "Samuel"
+    let contraseña = "1234"
+
+    if (usuario == "Samuel" && contraseña == "1234") {
+        console.log("usuario y contraseña correctos")
+    }
+
 // 3. Verifica si un número es positivo, negativo o cero e imprime un mensaje
+    let num = 0
+    
+    if (num > 0) {
+        console.log("El numero es positivo.")
+    } else if (num < 0) {
+        console.log("El numero es negativo.")
+    } else {
+        console.log("El numero es cero.")
+    }
 
 // 4. Verifica si una persona puede votar o no (mayor o igual a 18) e indica cuántos años le faltan
+
+    let edad = 15
+
+    if (edad >= 18){
+        console.log("Ya puedes votar!")
+    } else {
+        console.log("Aun no puedes votar, te quedan " + (18 - edad) + " años.")
+    }
 
 // 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable
 //    dependiendo de la edad 
 
-// 6. Muestra en que estación del año nos encontramos dependiendo del valor de una variable "mes"
+    let asignarEdad = edad >= 18 ? "adulto" : "menor"
+    
+    console.log(asignarEdad)
+
+// 6. Muestra en que estación del año nos encontramos dependiendo del valor de una variable "numMes"
+
+    let numMes = 1
+    let estacionDelAño
+
+    if (numMes == 12 || numMes == 1 || numMes == 2){
+        estacionDelAño = "Invierno"
+
+    } else if (numMes > 2 && numMes <=5){
+        estacionDelAño = "Primavera"
+
+    } else if (numMes > 5 && numMes <=8){
+        estacionDelAño = "Verano"
+
+    } else if (numMes > 8 && numMes < 12) {
+        estacionDelAño = "Otoño"
+    
+    } else {
+        estacionDelAño = "Pa mi que ese mes no existe, hay 12 nama"
+    }
+
+    console.log(estacionDelAño)
 
 // 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior
 
-// switch
+    if (numMes == 1 || numMes == 3 || numMes == 5 || numMes == 7 || numMes == 8 || numMes == 10 || numMes == 12) {
+        diasQueTiene = 31
+    
+    } else if (numMes == 4 || numMes == 6 || numMes == 9 || numMes == 11) {
+        diasQueTiene = 30
+    
+    } else if (numMes == 2) {
+        diasQueTiene = 28
 
+    } else {
+        console.log("Pa mi que ese mes no existe, hay 12 nama")
+    }
+
+    console.log("El mes " + numMes + " tiene " + diasQueTiene + " dias.")
+
+// switch
 // 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
+
+    let idioma = "Ingles", saludo
+
+    switch (idioma) {
+        case "Ingles": 
+            saludo = "Hello"
+            break
+        case "Español":
+            saludo = "Hola"
+            break
+        default:
+            saludo = "El Idioma no esta en nuestra base de datos."
+            break
+    }
+
+    console.log(saludo)
 
 // 9. Usa un switch para hacer de nuevo el ejercicio 6
 
+switch (numMes) {
+    case 12:
+    case 1:
+    case 2:
+        estacionDelAño = "Invierno"
+        break
+    case 3:
+    case 4:
+    case 5:
+        estacionDelAño = "Primavera"
+        break
+    case 6:
+    case 7:
+    case 8:
+        estacionDelAño = "Verano"
+        break
+    case 9:
+    case 10:
+    case 11:
+        estacionDelAño = "Otoño"
+        break
+    default:
+        estacionDelAño = "Pa mi que ese mes no existe, hay 12 nama"
+}
+
+console.log(estacionDelAño)
+
 // 10. Usa un switch para hacer de nuevo el ejercicio 7
+
+let diasQueTiene
+
+switch (numMes) {
+    case 1:
+        diasQueTiene = 31
+        break
+    case 2: 
+        diasQueTiene = 28
+        break
+    case 3:
+        diasQueTiene = 31
+        break
+    case 4: 
+        diasQueTiene = 30
+        break
+    case 5:
+        diasQueTiene = 31
+        break
+    case 6: 
+        diasQueTiene = 30
+        break
+    case 7:
+        diasQueTiene = 31
+        break
+    case 8: 
+        diasQueTiene = 31
+        break
+    case 9:
+        diasQueTiene = 30
+        break
+    case 10: 
+        diasQueTiene = 31
+        break
+    case 11:
+        diasQueTiene = 30
+        break
+    case 12: 
+        diasQueTiene = 31
+        break
+    default:
+        console.log("Pa mi que ese mes no existe, hay 12 nama")
+        break
+}
+
+console.log("El mes " + numMes + " tiene " + diasQueTiene + " dias.")
